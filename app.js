cc985759c656176be9932d793daaaebd92f66037
Ambu Karthik <thermosphere453@gmail.com>
2021-01-09 09:09:22 +0530

    new route added

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express()
 const port = 5000
 
 app.use(express.static('./'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Server running yay!')   
+})
+
+app.post('/getCookies', async (req, res) => {
+    const body = req.body
+    console.log(body)
+    res.json({ok: true, msg: 'Data received'})
 })
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
